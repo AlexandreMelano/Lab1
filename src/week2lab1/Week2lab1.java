6fd18fd1117b779a52a69e2772b2c7b53230f1c7
User1 <User1@10.250.12.193>
2017-01-18 14:43:42 -0500

    Small Change

--- src/week2lab1/Week2lab1.java
+++ src/week2lab1/Week2lab1.java
@@ -16,6 +16,7 @@ public class Week2lab1 {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(" New Additions");
     }
     
 }
